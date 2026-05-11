8ec6ac561719b6ad292c4c93314a4ccb24e1c36e
Aloise Dijoux <aloise.dijoux@lkb.upmc.fr>
2026-05-11 09:25:15 +0200

    File defining the ADCModule class: virtual base class to define the common interface for SIS3316 as well as SIS3315

---
+++ ADCModule.h
@@ -0,0 +1,16 @@
+/*
+class based on 
+*/
+
+class ADCModule {
+/*
+une classe configurable via un ensemble fermé de paramtres bien définis.
+Elle modélise un objet avec : 
+- état/config possible (num class)
+- moyens de modifier ces éttas
+- comportements dépendant de ces états
+*/
+
+
+
+};
