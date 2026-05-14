ea38360c2226f588a6dd85701bb34e75116e8569
Aloise Dijoux <aloise.dijoux@lkb.upmc.fr>
2026-05-14 15:56:29 +0200

    Add SIS3316Module class definition with overloaded constructors and Connect method

---
+++ SIS3316Module.h
@@ -0,0 +1,28 @@
+#ifndef SIS3316MODULE_H
+#define SIS3316MODULE_H
+#include "ADCModule.h"
+#include "drivers/sis3316_class.h"
+#include "ITransport.h"
+// "overloaded" = même nom, signatures différentes
+// Le compilateur choisit lequel appeler selon les arguments
+
+class SIS3316Module : public ADCModule {
+public:
+    // Surcharge 1 — Ethernet implicite
+    explicit SIS3316Module(const std::string& ip);
+
+    // Surcharge 2 — transport injecté explicitement
+    explicit SIS3316Module(const std::string& ip,
+                            std::shared_ptr<ITransport> transport);
+
+    // Surcharge 3 — slot VME
+    explicit SIS3316Module(uint32_t baseAddress, int crateHandle);
+
+    void ADCModule::Connect() override {
+        // code de connexion spécifique au SIS3316
+    };
+};
+
+
+
+#endif // SIS3316MODULE_H
